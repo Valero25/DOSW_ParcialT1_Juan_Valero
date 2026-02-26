b27f1f545dcab4f30058c25178f316a5d0a02d7c
Valero25 <juandavidvaleroa@gmail.com>
2026-02-26 08:24:16 -0500

    Creacion de carpetas

---
+++ src/test/java/edu/dosw/parcial/AppTest.java
@@ -0,0 +1,13 @@
+package edu.dosw.parcial;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testAppInstantiation() {
+        App app = new App();
+        assertNotNull(app);
+    }
+}
